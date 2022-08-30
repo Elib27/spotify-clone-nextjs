60f42dcf92648309ff2e7fba252ce2cab8a6fb61
Eliot Bas <eliot.bas@orange.fr>
2022-08-30 18:59:12 +0200

    continue search page

---
+++ components/SearchCard.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+import Image from 'next/image'
+
+const Container = styled.div`
+  ${({ isBigCard }) => isBigCard ?
+  `height: 220px;
+   width: clamp(340px, 50%, 480px);
+   flex-shrink: 0;
+   `
+   : 
+   `width: 100%;
+    aspect-ratio: 1;`
+  }
+  background-color: ${({cardBackgroundColor}) => cardBackgroundColor};
+  border-radius: 8px;
+  position: relative;
+  cursor: pointer;
+  overflow: hidden;
+`
+const CardTitle = styled.h3`
+  ${({ isBigCard }) => isBigCard ? 'font-size: 2.5rem;' : 'font-size: 1.5rem;'}
+  letter-spacing: -0.04rem;
+  font-weight: 700;
+  user-select: none;
+  color: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 16px;
+  z-index: 2;
+`
+const CardImage = styled.div`
+  ${({ isBigCard }) => isBigCard ?
+    `
+      height: 128px;
+      width: 128px;
+    `
+    :
+    `
+    height: 100px;
+    width: 100px;
+    `
+  }
+  
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+  -webkit-box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+  transform: rotate(25deg) translate(18%,-2%);
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  z-index: 1;
+`
+
+export default function SearchCard({title, cardBackgroundColor, isBigCard, imageSrc}) {
+  return (
+    <Container
+      isBigCard={isBigCard}
+      cardBackgroundColor={cardBackgroundColor}
+    >
+      <CardTitle isBigCard={isBigCard}>
+        {title}
+      </CardTitle>
+      <CardImage isBigCard={isBigCard}>
+        <Image src={imageSrc} alt={title} layout="fill"/>
+      </CardImage>
+    </Container>
+  )
+}
--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,85 @@
+import styled from 'styled-components'
+import SearchCard from '../components/SearchCard'
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: #121212;
+  padding: 32px 16px 0 16px;
+`
+const SearchSection = styled.section`
+  padding: 16px 0;
+`
+const SectionTitle = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: #fff;
+  padding-bottom: 16px;
+`
+const MusicStyleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 18px;
+  overflow-x: scroll;
+  scroll-behavior: smooth;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+const CategoriesContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(158px, 1fr));
+  grid-gap: 24px;
+  position: relative;
+`
+
+const CategoryCards = [
+  {title: "Podcasts", backgroundColor: "#27856a", cover: "/categoryCover.jpg"},
+  {title: "Conçu spécialement pour vous", backgroundColor: "rgb(30, 50, 100)", cover: "/categoryCover.jpg"},
+  {title: "Classements", backgroundColor: "rgb(141, 103, 171)", cover: "/categoryCover.jpg"},
+  {title: "Podcasts", backgroundColor: "#27856a", cover: "/categoryCover.jpg"},
+  {title: "Podcasts", backgroundColor: "#27856a", cover: "/categoryCover.jpg"},
+  {title: "Podcasts", backgroundColor: "#27856a", cover: "/categoryCover.jpg"},
+  {title: "Podcasts", backgroundColor: "#27856a", cover: "/categoryCover.jpg"}
+]
 
 export default function search() {
   return (
-    <div>Search Page</div>
+    <Container>
+      <SearchSection>
+      <SectionTitle>Vos genres préférés</SectionTitle>
+      <MusicStyleContainer>
+        <SearchCard
+          title="Variété française"
+          isBigCard
+          cardBackgroundColor="#b49bc8"
+          imageSrc="/search_cover.jpg"
+        />
+        <SearchCard
+          title="Hip-Hop"
+          isBigCard
+          cardBackgroundColor="#ba5d07"
+          imageSrc="/search_cover2.jpg"
+        />
+      </MusicStyleContainer>
+      </SearchSection>
+      <SearchSection>
+        <SectionTitle>Parcourir tout</SectionTitle>
+        <CategoriesContainer>
+          {CategoryCards.map((category, index) => (
+            <SearchCard
+              title={category.title}
+              cardBackgroundColor={category.backgroundColor}
+              imageSrc={category.cover}
+              key={category.title}
+            />
+          ))}
+        </CategoriesContainer>
+      </SearchSection>
+    </Container>
   )
 }
