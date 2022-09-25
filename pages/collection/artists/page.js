288f793432d3dd5d0ccab76da43b4497a3ee9f49
Eliot Bas <eliot.bas@orange.fr>
2022-09-25 22:11:17 +0200

    update next to 12.3.1

--- pages/collection/artists/page.js
+++ pages/collection/artists/page.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
-import CollectionPageContainer from "../../components/collection/CollectionPageContainer"
-import PlaylistCard from "../../components/collection/PlaylistCard"
+import CollectionPageContainer from "../../../components/collection/CollectionPageContainer"
+import PlaylistCard from "../../../components/collection/PlaylistCard"
 
 export default function Artists() {
   return (
