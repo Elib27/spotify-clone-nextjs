3572aa22d07d2d4730ab06a25cf2b9d2d208d1ed
Eliot Bas <eliot.bas@orange.fr>
2023-03-01 21:14:05 +0100

    add title and meta description for all pages

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,10 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='fr'>
-        <Head />
+        <Head>
+          <title>Spotify Clone</title>
+          <meta name='description' content='Spotify clone coded by BAS Eliot with Next JS.' />
+        </Head>
         <body>
           <Main />
           <NextScript />
