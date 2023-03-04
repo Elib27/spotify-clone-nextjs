6ef09e43dd5a8e89696a6ac745bce4d780c4ad8c
Eliot Bas <eliot.bas@orange.fr>
2023-03-04 12:12:59 +0100

    remove button inside a

--- components/searchPage/CategoryFilterButton.js
+++ components/searchPage/CategoryFilterButton.js
@@ -5,14 +5,13 @@ const FilterButtonWrapper = styled.div`
   padding: 2px;
   flex-shrink: 0;
 `
-const FilterButton = styled.button`
+const FilterButton = styled.div`
   font-size: 0.875rem;
   font-weight: 400;
   white-space: nowrap;
   padding: 4px 12px;
   color: #fff;
   border-radius: 50px;
-  border: none;
   background-color: #232323;
   transition: background-color 0.2s ease 0s, color 0.2s ease 0s;
   line-height: 1.72;
