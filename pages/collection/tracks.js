db6110b9f1c0cae96c946edf5aef16ce88eb32c5
Eliot Bas <eliot.bas@orange.fr>
2023-03-04 12:53:49 +0100

    remove unused styling

--- pages/collection/tracks.js
+++ pages/collection/tracks.js
@@ -24,26 +24,6 @@ const BackgroundGradient = styled.div`
   z-index: -1;
   pointer-events: none;
 `
-const PlayMusicSection = styled.section`
-  padding: 24px 32px;
-  position: relative;
-`
-const PlayButton = styled.button`
-  height: 56px;
-  width: 56px;
-  background-color: #1ed760;
-  border-radius: 50%;
-  outline: 0;
-  border: 0;
-  color: #000;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #000;
-  &:hover {
-    transform: scale(1.04);
-  }
-`
 const TracksWrapper = styled.div`
   padding: 0 32px;
 `
