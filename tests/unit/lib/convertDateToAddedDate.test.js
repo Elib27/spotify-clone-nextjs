a6c95d6ec4f4dcfcc9bb39a304345db09760e2e6
Eliot Bas <eliot.bas@orange.fr>
2023-08-25 18:44:10 +0200

    add jest and tests

---
+++ tests/unit/lib/convertDateToAddedDate.test.js
@@ -0,0 +1,36 @@
+import convertDateToAddedDate from '../../../lib/convertDateToAddedDate'
+
+describe('convertDateToAddedDate', () => {
+  it('should return the correct formatted date', () => {
+    expect(convertDateToAddedDate('2017-05-26')).toBe('26 mai 2017')
+  });
+  it('should return the correct formatted date', () => {
+    expect(convertDateToAddedDate('2022-11-07')).toBe('7 nov. 2022')
+  });
+  it('should return the correct formatted date', () => {
+    expect(convertDateToAddedDate('2003-01-01')).toBe('1 janv. 2003')
+  });
+  it('should convert a date to "il y a x heures" when between 1 hour and 1 day ago', () => {
+    const currentDate = new Date();
+    const pastDate = new Date(currentDate - 5 * 60 * 60 * 1000); // 5 hours ago
+    const result = convertDateToAddedDate(pastDate.toISOString());
+    const expected = `il y a 5 heures`;
+    expect(result).toEqual(expected);
+  });
+
+  it('should convert a date to "il y a x minutes" when between 1 minute and 1 hour ago', () => {
+    const currentDate = new Date();
+    const pastDate = new Date(currentDate - 5 * 60 * 1000); // 5 minutes ago
+    const result = convertDateToAddedDate(pastDate.toISOString());
+    const expected = `il y a 5 minutes`;
+    expect(result).toEqual(expected);
+  });
+
+  it('should convert a date to "il y a x secondes" when less than 1 minute ago', () => {
+    const currentDate = new Date();
+    const pastDate = new Date(currentDate - 5000); // 5 seconds ago
+    const result = convertDateToAddedDate(pastDate.toISOString());
+    const expected = `il y a 5 secondes`;
+    expect(result).toEqual(expected);
+  });
+})
---
+++ tests/unit/lib/levenshteinDistance.test.js
@@ -0,0 +1,23 @@
+import levenshteinDistance from '../../../lib/levenshteinDistance'
+
+describe('levenshteinDistance', () => {
+  it('calculates the correct Levenshtein distance between two equal words', () => {
+    expect(levenshteinDistance('kitten', 'kitten')).toEqual(0);
+  });
+
+  it('calculates the correct Levenshtein distance between words with one character difference', () => {
+    expect(levenshteinDistance('kitten', 'sitten')).toEqual(1);
+  });
+
+  it('calculates the correct Levenshtein distance between words with multiple character differences', () => {
+    expect(levenshteinDistance('kitten', 'sitting')).toEqual(3);
+  });
+
+  it('calculates the correct Levenshtein distance for words with different casing', () => {
+    expect(levenshteinDistance('Hello', 'hello')).toEqual(0);
+  });
+
+  it('calculates the correct Levenshtein distance for words with different lengths', () => {
+    expect(levenshteinDistance('short', 'longerword')).toEqual(8);
+  });
+});
